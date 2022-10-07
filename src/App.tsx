4cd1d74d53f8a58353b7eb86ce5ebe05b08cde1c
João Padilha <jvlpadilha@outlook.com>
2022-10-07 15:45:10 -0300

    Create and configure lightTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 function App() {
   return (
-    <BrowserRouter>
-        <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
-import {Routes, Navigate, Route} from 'react-router-dom'
+import { Routes, Navigate, Route } from 'react-router-dom';
+import { Button } from '@mui/material';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/pagina-inicial' element={<p>Página Inicial</p>}/>
-      <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Teste</Button>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
-  )
-}
+  );
+};
