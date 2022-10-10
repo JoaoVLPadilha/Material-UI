c86989af97fb20d8ec7fc0d11c86d1ff6183bdb9
João Padilha <jvlpadilha@outlook.com>
2022-10-10 10:50:42 -0300

    Change Folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { AppRoutes } from './routes';
+import FirstPage from './shared/components/FirstPage';
 import MenuLateral from './shared/components/menu-lateral/MenuLateral';
 import { AppDrawerProvider, AppThemeProvider } from './shared/contexts';
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <AppDrawerProvider>
         <BrowserRouter>
           <MenuLateral>
+            <FirstPage/>
             <AppRoutes />
           </MenuLateral>
         </BrowserRouter>
