673955390e28858e0fe3264a82e73f264353c76c
João Padilha <jvlpadilha@outlook.com>
2022-10-14 20:06:47 -0300

    All button in toolbat

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import ListTools from '../../shared/components/list-tools/ListTools'
+import { DetailTools } from '../../shared/components'
 import BaseLayout from '../../shared/layouts/BaseLayout'
-
 const Dashboard: React.FC = () => {
   return (
     <BaseLayout 
     title='Home Page' 
     listTools={
-      <ListTools
-      showSearchInput
-      />
+      <DetailTools></DetailTools>
     }
     >
       Testando
