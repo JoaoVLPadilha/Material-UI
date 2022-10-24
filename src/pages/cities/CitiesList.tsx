a4e05453c7a00bd647a5ad5b6a467720b2b2603d
João Padilha <jvlpadilha@outlook.com>
2022-10-24 19:53:06 -0300

    Citie query

--- src/pages/cities/CitiesList.tsx
+++ src/pages/cities/CitiesList.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import ListTools from '../../shared/components/list-tools/ListTools'
+import BaseLayout from '../../shared/layouts/BaseLayout'
 
-const CitiesList = () => {
+const CitiesList: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = React.useMemo(() => {
+    console.log(searchParams.get('search'))
+    return searchParams.get('search') || '';
+  }, [searchParams]);
+
   return (
-    <div>CitiesList</div>
+    <BaseLayout 
+    title='Cities List'
+    listTools={<ListTools
+    showSearchInput
+    textSearch={busca}
+    textButtonNew='Nova'
+    onChangeTextSearch={text => setSearchParams({search: text}, { replace: true})}
+
+    />}
+    ></BaseLayout>
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import { Routes, Navigate, Route } from 'react-router-dom';
 import { useAppDrawerContext } from '../shared/contexts';
 import Dashboard from '../pages/dashboard/Dashboard';
+import CitiesList from '../pages/cities/CitiesList';
 export const AppRoutes = () => {
   const {setDrawerOptions } = useAppDrawerContext();
   React.useEffect(() =>{
     setDrawerOptions([
       {
-        label: 'Home Page',
         icon: 'home',
-        path: 'pagina-inicial'
+        path: 'pagina-inicial',
+        label: 'Home Page',
       },
       {
-        label: 'Star Page',
         icon: 'star',
-        path: 'star page'
+        path: 'star page',
+        label: 'Star Page',
+      },
+      {
+        icon: 'location_city',
+        path: '/cities',
+        label: 'Cities',
       },
     ])
   }, [])
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard/>} />
+      <Route path="/cities" element={<CitiesList/>} />
+      {/* <Route path="/cities/detail/:id" element={<CitiesList/>} /> */}
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
