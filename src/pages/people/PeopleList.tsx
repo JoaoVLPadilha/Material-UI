915c61df190f8b56f431e10bc9062615a53811e4
Joao Victor Lopes Padilha <jvlpadilha@outlook.com>
2022-10-27 01:11:58 -0300

    Added Table

--- src/pages/people/PeopleList.tsx
+++ src/pages/people/PeopleList.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import ListTools from '../../shared/components/list-tools/ListTools';
+import BaseLayout from '../../shared/layouts/BaseLayout';
+import { useDebounce } from '../../shared/hooks';
 import {
-  Paper,
-  Skeleton,
+  IListPeople,
+  PeopleService,
+  TTotalCountPeople,
+} from '../../shared/services/api/people/PeopleService';
+import {
   Table,
   TableBody,
   TableCell,
@@ -9,21 +16,12 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material';
-import { useSearchParams } from 'react-router-dom';
-
-import {
-  IListPeople,
-  PeopleService,
-} from '../../shared/services/api/people/PeopleService';
-import ListTools from '../../shared/components/list-tools/ListTools';
-import BaseLayout from '../../shared/layouts/BaseLayout';
-import { useDebounce } from '../../shared/hooks/';
 
 const PeopleList: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { debounce } = useDebounce(1000);
 
-  const [rows, setRows] = React.useState<IListPeople[]>();
+  const [rows, setRows] = React.useState<IListPeople[]>([]);
   const [totalCount, setTotalCount] = React.useState(0);
   const [isLoading, setIsLoading] = React.useState(true);
 
@@ -37,12 +35,11 @@ const PeopleList: React.FC = () => {
       PeopleService.getAll(1, search).then((result) => {
         setIsLoading(false);
         console.log(result);
-
         if (result instanceof Error) alert(result.message);
         else {
-          console.log(result.data);
-          setTotalCount(result.totalCount);
+          console.log(result);
           setRows(result.data);
+          setTotalCount(result.totalCount);
         }
       });
     });
@@ -62,31 +59,14 @@ const PeopleList: React.FC = () => {
         />
       }
     >
-      <TableContainer
-        component={Paper}
-        variant="outlined"
-        sx={{ m: 1, width: 'auto' }}
-      >
+      <TableContainer>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell>Actions</TableCell>
-              <TableCell>Full Name</TableCell>
-              <TableCell>E-mail</TableCell>
             </TableRow>
           </TableHead>
-
-          <TableBody>
-            {rows?.map((row) => {
-              return (
-                <TableRow key={row.id}>
-                  <TableCell>Actions</TableCell>
-                  <TableCell>{row.lastName}</TableCell>
-                  <TableCell>{row.email}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
+          <TableBody></TableBody>
         </Table>
       </TableContainer>
     </BaseLayout>
